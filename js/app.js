5793d7d1cba5bb8b7efa52da339e0287fc5a01ce
Oana Mancu <oanamancu@gmail.com>
2024-11-06 01:51:22 +0200

    feat: add active-link class to nav li when scrolling

--- js/app.js
+++ js/app.js
@@ -11,24 +11,24 @@
  * 
  * JS Standard: ESlint
  * 
-*/
+ */
 
 /**
  * Comments should be present at the beginning of each procedure and class.
  * Great to have comments before crucial code sections within the procedure.
-*/
+ */
 
 /**
  * Define Global Variables
  * 
-*/
+ */
 const sections = document.querySelectorAll('section');
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
-*/
+ */
 
 
 
@@ -36,21 +36,21 @@ const sections = document.querySelectorAll('section');
  * End Helper Functions
  * Begin Main Functions
  * 
-*/
+ */
 
 // build the nav
-
 function buildNav() {
     const ul = document.querySelector('#navbar__list');
 
-    sections.forEach ( section => {
-        const sectionNumber = section.id.charAt(section.id.length-1); 
+    sections.forEach((section, i) => {
+        const sectionNumber = section.id.charAt(section.id.length - 1);
         const li = document.createElement('li');
         li.classList.add('navbar__menu');
         li.setAttribute('id', `li${sectionNumber}`);
         const menuLink = document.createElement('a');
         menuLink.classList.add('navbar__menu', 'menu__link');
         menuLink.textContent = `Section ${sectionNumber}`;
+        i === 0 ? menuLink.classList.add("active__link") : "";
         li.appendChild(menuLink);
         ul.appendChild(li);
     });
@@ -59,20 +59,23 @@ function buildNav() {
 }
 
 // Add class 'active' to section when near top of viewport
-function makeActive(){
-    for (const section of sections) {
+function makeActive() {
+    sections.forEach((section, i) => {
+        const navLinks = document.querySelectorAll('.menu__link')
         const box = section.getBoundingClientRect();
         //Find a value that works best, but 150 seems to be a good start.
         if (box.top <= 150 && box.bottom >= 150) {
             //apply active state on current section and corresponding Nav link
-            if (!section.classList.contains('awesome-active-class') ) {
+            if (!section.classList.contains('awesome-active-class')) {
                 section.classList.add('awesome-active-class');
             }
+            navLinks[i].classList.add('active__link');
         } else {
             //Remove active state from other section and corresponding Nav link
             section.classList.remove('awesome-active-class');
+            navLinks[i].classList.remove('active__link');
         }
-     }
+    });
 }
 
 
@@ -84,7 +87,7 @@ function scrollToAnchor(event) {
         const section = document.querySelector(`#section${target.id.charAt(target.id.length-1)}`);
         window.scrollTo({
             top: section.offsetTop,
-            behavior:"smooth"
+            behavior: "smooth"
         });
     }
 }
@@ -93,12 +96,12 @@ function scrollToAnchor(event) {
  * End Main Functions
  * Begin Events
  * 
-*/
+ */
 
 // Build menu 
 buildNav();
 
 // Set sections as active
-document.addEventListener("scroll", function() { makeActive();});
-
-
+document.addEventListener("scroll", function () {
+    makeActive();
+});
