bba16e822684c9cfb204e42072f5b06f466ea0ad
Oana Mancu <oanamancu@gmail.com>
2024-11-05 18:03:10 +0200

    feat: build nav menu

--- js/app.js
+++ js/app.js
@@ -40,8 +40,29 @@
 
 // build the nav
 
+function buildNav() {
+    const ul = document.querySelector('#navbar__list');
+
+    for (let i = 0; i < document.querySelectorAll('section').length; i++) {
+        const li = document.createElement('li');
+        li.classList.add('navbar__menu');
+        const menuLink = document.createElement('a');
+        menuLink.classList.add('navbar__menu', 'menu__link');
+        menuLink.textContent = `Section ${i+1}`;
+        li.appendChild(menuLink);
+        ul.appendChild(li);
+    }
+
+}
 
 // Add class 'active' to section when near top of viewport
+
+function sectionSelect(event) {
+    const target = event.target;
+    if (target.nodeName === 'SECTION') {
+
+    }
+}
 
 
 // Scroll to anchor ID using scrollTO event
@@ -54,6 +75,7 @@
 */
 
 // Build menu 
+buildNav();
 
 // Scroll to section on link click
 
